c0a316565d56c9edc271e81f64bdcc0925ff5ba3
Carlos Gacitua <carlitos.gacitua@gmail.com>
2023-11-07 11:21:09 -0300

    arreglo proyecto

---
+++ estacionamiento/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EstacionamientoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'estacionamiento'
---
+++ transaccion_pago/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TransaccionPagoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'transaccion_pago'
