416e7a13149f9e88dac6e53d115f79780e3a54d6
Diego Ruminot <diegoruminot12@gmail.com>
2023-11-09 18:19:54 -0300

    Implementación de página Index-Cliente

---
+++ cliente/static/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        prevArrow: $('.slick-prev'),
+        nextArrow: $('.slick-next'),
+    });
+});
--- cliente/urls.py
+++ cliente/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('',views.index,name='index'),
+    path('index/',views.index,name='index'),
+    #path('html/cartelera/',views.cartelera,name='cartelera'),
+    #path('html/index/',views.index, name="index"),
 ]
