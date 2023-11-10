5ab7b723a597cf2dfc6d9e6f786f13d9585dd4a0
Diego Ruminot <diegoruminot12@gmail.com>
2023-11-10 12:29:21 -0300

    añadiendo vista pago, registro

--- cliente/urls.py
+++ cliente/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('index/',views.index,name='index'),
+    path('pago/',views.pago,name='pago'),
+    path('registro/',views.registro,name='registro'),
     #path('html/cartelera/',views.cartelera,name='cartelera'),
     #path('html/index/',views.index, name="index"),
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def pago(request):
+    return render(request,'pago.html')
+
+def registro(request):
+    return render(request,'registro.html')
