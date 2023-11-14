da6a0694ca73ccb61da2cce92360f065476dce3f
Joaquin <joa.calderonm@duocuc.cl>
2023-11-14 15:53:54 -0300

    GPS 1.4.1

--- geolocalizacion/views.py
+++ geolocalizacion/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Puntointeres
 
 # Create your views here.
 
 def gps(request):
-    return render (request, 'gps.html')
+    puntos_interes = Puntointeres.objects.all()
+
+    # Pasa los puntos de interés a la plantilla
+    return render(request, 'gps.html', {'puntos_interes': puntos_interes})
