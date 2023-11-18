0609b21674980df2221fc7d1586f03b19b1f58fd
Joaquin <joa.calderonm@duocuc.cl>
2023-11-18 18:30:07 -0300

    model gps

--- geolocalizacion/views.py
+++ geolocalizacion/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from .models import Puntointeres
-=======
-from .models import *
->>>>>>> 696b6bebebad010afbad8985f0e55eccbfb5d77e
 
 # Create your views here.
 
 def gps(request):
     puntos_interes = Puntointeres.objects.all()
-<<<<<<< HEAD
-
     # Pasa los puntos de interés a la plantilla
     return render(request, 'gps.html', {'puntos_interes': puntos_interes})
-=======
-    return render (request, 'gps.html', {'punto_intereses': puntos_interes})
->>>>>>> 696b6bebebad010afbad8985f0e55eccbfb5d77e
