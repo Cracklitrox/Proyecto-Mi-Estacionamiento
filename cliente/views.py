473dc49d03f486b15651dab2ef0d7b7507eb9390
Joaquin <joa.calderonm@duocuc.cl>
2023-11-23 12:34:15 -0300

    mi-estacionamiento

--- cliente/views.py
+++ cliente/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from geolocalizacion.models import Puntointeres
+from estacionamiento.models import Estacionamiento
 
 # Create your views here.
 def indexCliente(request):
-    return render(request,'indexCliente.html')
+    puntos_interes = Puntointeres.objects.all()
+    estacionamiento = Estacionamiento.objects.all()
+    return render(request,'indexCliente.html', {'puntos_interes': puntos_interes,
+                                                'estacionamiento': estacionamiento})
 
 def pagoCliente(request):
     return render(request,'pagoCliente.html')
 
 def registroCliente(request):
+    
     return render(request,'registroCliente.html')
