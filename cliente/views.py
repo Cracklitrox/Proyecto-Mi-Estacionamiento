d29d551cc5d05f70940bcd5a018f31765ed2b4d8
Carlos Gacitua <carlitos.gacitua@gmail.com>
2023-11-24 15:05:59 -0300

    login del usuario listo

--- cliente/views.py
+++ cliente/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import Cliente
 
 # Create your views here.
 def indexCliente(request):
@@ -11,4 +14,30 @@ def registroCliente(request):
     return render(request,'registroCliente.html')
 
 def loginCliente(request):
-    return render(request, 'loginCliente.html')
+    if request.method == "POST":
+        campoNombre = request.POST["campoNombre"]
+        campoContrasena = request.POST["campoContrasena"]
+        flagCorreo = False
+        if campoNombre.endswith("@gmail.com"):
+            flagCorreo = True
+        try:
+            if flagCorreo:
+                cliente = Cliente.objects.get(
+                    correoelectronico = campoNombre,
+                    contrasena = campoContrasena
+                )
+            else:
+                cliente = Cliente.objects.get(
+                    nombreusuario = campoNombre,
+                    contrasena = campoContrasena
+                )
+            user = authenticate(request, username=cliente.nombreusuario, password=campoContrasena)
+            if user:
+                login(request, user)
+                return redirect('indexCliente')
+        except Cliente.DoesNotExist:
+            mensajeAdvertencia = "No se ha encontrado el nombre de usuario, correo o la contraseña. Por favor, inténtelo nuevamente."
+            messages.warning(request, mensajeAdvertencia)
+            return render(request, "loginCliente.html")
+    else:
+        return render(request, "loginCliente.html")
