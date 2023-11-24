8b2fe715d3947f48a1fb88d2ecfe2d5e3835b9bb
joacalderonm <joa.calderonm@duocuc.cl>
2023-11-24 15:08:21 -0300

    dueño + estacionamientos

--- cliente/models.py
+++ cliente/models.py
@@ -16,14 +16,15 @@ class Cliente(models.Model):
     correoelectronico = models.CharField(db_column='correoElectronico', max_length=65, verbose_name='Correo Electronico')
     telefono = models.IntegerField(verbose_name='Telefono')
     nombreusuario = models.CharField(db_column='nombreUsuario', max_length=20, verbose_name='Nombre de Usuario')
-    contrasena = models.CharField(max_length=128)
+    contrasena = models.CharField(max_length=30)
     direccion = models.CharField(max_length=255)
     calificacionpromedio = models.DecimalField(db_column='calificacionPromedio', max_digits=2, decimal_places=1, verbose_name='Calificacion Promedio', blank=True, null=True)
     activo = models.BooleanField(default=True, verbose_name='Cliente Activo')
+    imagen = models.ImageField(upload_to='fotoCliente/', null=True, blank=True, verbose_name='Imagen del Cliente')
     id_tarjetacredito = models.ForeignKey('transaccion_pago.Tarjetacredito', models.DO_NOTHING, db_column='id_tarjetaCredito', verbose_name='Tarjeta de Credito')
     id_banco = models.ForeignKey(Banco, models.DO_NOTHING, db_column='id_banco', verbose_name='Banco')
     id_comuna = models.ForeignKey('usuario.Comuna', models.DO_NOTHING, db_column='id_comuna', verbose_name='Comuna')
-
+    
     def save(self, *args, **kwargs):
         if not self.id or self.contrasena != Cliente.objects.get(id=self.id).contrasena:
             self.contrasena = make_password(self.contrasena);
--- miEstacionamiento/settings.py
+++ miEstacionamiento/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/"media"
 # Application definition
 
 INSTALLED_APPS = [
