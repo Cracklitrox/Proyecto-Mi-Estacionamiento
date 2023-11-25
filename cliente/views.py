400746e30607ba035e4c134daa0817d89182c1e5
Joaquin <joa.calderonm@duocuc.cl>
2023-11-25 01:35:21 -0300

    Arriendo-casillas-vinculado

--- cliente/views.py
+++ cliente/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .models import Cliente
 from geolocalizacion.models import Puntointeres
-from estacionamiento.models import Estacionamiento
+from estacionamiento.models import *
+from django.views import View
+from django.http import JsonResponse
 from arriendo.models import *
 from arriendo.form import *
 
@@ -53,7 +55,21 @@ def loginCliente(request):
         return render(request, "loginCliente.html")
 
 def estacionamientos(request):
+    casilla = Casilla.objects.all()
     context = {
-        
+        'casilla':casilla,
         }
     return render(request,'estacionamientos.html',context )
+
+class GuardarEstadoCasillaView(View):
+    def post(self, request, *args, **kwargs):
+        id_casilla = request.POST.get('idCasilla')
+        nuevo_estado = request.POST.get('nuevoEstado')
+
+        # Actualiza el estado de la casilla en la base de datos
+        casilla = Casilla.objects.get(id=id_casilla)
+        casilla.disponible = nuevo_estado
+        casilla.save()
+
+        # Responde con un JSON para indicar el éxito de la operación si es necesario
+        return JsonResponse({'success': True})
