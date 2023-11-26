8ef59ec6b72e2c02419a6bf5c2e3528abd7f8c1e
Carlos Gacitua <carlitos.gacitua@gmail.com>
2023-11-25 21:55:00 -0300

    atributo imagen agregado en add estacionamiento y visualizacion en index Dueno

--- dueno/urls.py
+++ dueno/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 from estacionamiento.views import cambiar_estado
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('index/',views.indexDueno,name='index'),
@@ -9,6 +12,7 @@ urlpatterns = [
     path('index/addEstacionamiento/',views.addEstacionamiento,name='addEstacionamiento'),
     path('eliminarEstacionamiento/<int:id>',views.eliminarEstacionamiento,name='eliminarEstacionamiento'),
     path('editEstacionamiento/<int:id>',views.editEstacionamiento,name='editEstacionamiento'),
-
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
