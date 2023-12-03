f490895d206bbdd956f8dbc888c5e484a7567e19
Carlos Gacitua <carlitos.gacitua@gmail.com>
2023-12-03 16:13:22 -0300

    Funcion CRUD completa en pagina 'bancos'

--- administrador/urls.py
+++ administrador/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bancos/agregarBanco/',views.agregarBanco, name='agregarBanco'),
-    path('bancos/listarBanco/', views.listarBanco, name='listarBanco')
+    path('bancos/agregarBanco/', views.agregarBanco, name='agregarBanco'),
+    path('bancos/listarBanco/', views.listarBanco, name='listarBanco'),
+    path('bancos/modificarBanco/<id>/', views.modificarBanco, name='modificarBanco'),
+    path('bancos/eliminarBanco/<id>/', views.eliminarBanco, name='eliminarBanco')
 ]
--- administrador/views.py
+++ administrador/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BancoForm
 from transaccion_pago.models import Banco
 
@@ -26,3 +26,26 @@ def listarBanco(request):
         'bancos': bancos
     }
     return render(request, 'bancos/listarBanco.html', context)
+
+def modificarBanco(request, id):
+    banco = get_object_or_404(Banco, id=id)
+
+    context = {
+        'form':BancoForm(instance=banco)
+    }
+
+    if request.method == 'POST':
+        formulario = BancoForm(data = request.POST, instance=banco, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect(to='listarBanco')
+        else:
+            context["form"] = formulario
+            context["mensaje_incorrecto"] = "No se ha podido modificar el banco."
+
+    return render(request, 'bancos/modificarBanco.html', context)
+
+def eliminarBanco(request, id):
+    banco = get_object_or_404(Banco, id=id)
+    banco.delete()
+    return redirect(to='listarBanco')
